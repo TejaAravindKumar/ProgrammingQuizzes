1dc8c69dc8cb4ad35f0173d0362d84025a7e8e7d
CodexBox <teja.aravindkumar@gmail.com>
2022-08-08 18:52:31 +0530

    Commit by teja O

--- src/AssesmentsPractice/A0808/CharacterCount.java
+++ src/AssesmentsPractice/A0808/CharacterCount.java
@@ -7,12 +7,11 @@ import java.util.stream.IntStream;
 
 public class CharacterCount {
     public static void main(String[] args) {
-        IntStream chars = "dazzlings treees hi".chars();
+        IntStream chars = "dazz treees hi".chars();
         Map<Character,Integer> map = new HashMap<>();
         chars.forEach(e->{
-            int count = 0;
             if (!map.containsKey((char)e)) {
-                map.put((char)e,count++);
+                map.put((char)e,1);
             }
             map.put((char) e, map.get((char) e) + 1);
         });
---
+++ src/leetcode/arrays/NumbersSmallerThanCurrentNumber.java
@@ -0,0 +1,26 @@
+package leetcode.arrays;
+
+import java.util.Arrays;
+
+public class NumbersSmallerThanCurrentNumber {
+    public static void main(String[] args) {
+        int[] nums = {7,7,7,7};
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
+
+    }
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] arr = new int[nums.length];
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            count = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (nums[i] > nums[j]){
+                    count++;
+                }
+            }
+            arr[i] = count;
+        }
+        return arr;
+
+    }
+}
